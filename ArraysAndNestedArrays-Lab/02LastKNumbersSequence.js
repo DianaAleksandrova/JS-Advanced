4ff651956f3b12c992c713a463b3ab3b7f723a97
DianaAleksandrova <104830664+DianaAleksandrova@users.noreply.github.com>
2022-10-11 15:41:37 +0300

    add

--- ArraysAndNestedArrays-Lab/02LastKNumbersSequence.js
+++ ArraysAndNestedArrays-Lab/02LastKNumbersSequence.js
@@ -3,9 +3,7 @@ function numbers(n, k){
     
         for (let i = 1; i < n; i++) {
             
-            let start = i - k < 0 
-            ? 0 
-            : i - k;
+            let start = i - k < 0 ? 0 : i - k;
             result.push(result.slice(start).reduce((acc, num) => acc + num));
         }
         return result;
