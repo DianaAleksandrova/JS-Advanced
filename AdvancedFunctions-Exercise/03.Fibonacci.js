efe67a4d6b4f452b36c06641c9e2dd53d877e1ad
DianaAleksandrova <104830664+DianaAleksandrova@users.noreply.github.com>
2022-10-12 15:42:28 +0300

    Added a source code

---
+++ AdvancedFunctions-Exercise/03.Fibonacci.js
@@ -0,0 +1,28 @@
+function fibonacci() {
+    let current = 0;
+    let next = 1;
+
+    return function() {
+        let result = current + next;
+        current = next;
+        next = result;
+
+        return current;
+    }
+}
+
+let fib = fibonacci();
+
+console.log(fib()); // 1
+
+console.log(fib()); // 1
+
+console.log(fib()); // 2
+
+console.log(fib()); // 3
+
+console.log(fib()); // 5
+
+console.log(fib()); // 8
+
+console.log(fib()); // 13
