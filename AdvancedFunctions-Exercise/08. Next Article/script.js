ae34891c700293064c1245b3df743d74608d9d39
DianaAleksandrova <104830664+DianaAleksandrova@users.noreply.github.com>
2022-10-14 13:02:01 +0300

    Added a source code

---
+++ AdvancedFunctions-Exercise/08. Next Article/script.js
@@ -0,0 +1,13 @@
+function getArticleGenerator(articles) {
+    let div = document.getElementById('content');
+
+    return function () {
+        let article = articles.shift();
+
+        if (article != undefined) {
+            let element = document.createElement('article');
+            element.textContent = article;
+            div.appendChild(element);
+        }
+    };
+}
